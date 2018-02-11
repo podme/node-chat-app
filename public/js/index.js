183f6b074a6e432e91909796adcc209f22bb9ac5
PaulODonovan <blueprintdesignmail@yahoo.com>
2018-02-11 19:11:01 +0100

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
 var socket = io();
+
+function scrollToBottom () {
+	// selectors
+	var messages = $('#messages'); 
+	var newMessage = messages.children('li:last-child');
+	// heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		console.log('should scroll');
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function () {
 	console.log('connected to server');
 	// socket.emit('createMessage', {
@@ -18,6 +36,7 @@ socket.on('newMessage', function (message) {
 		createdAt: formattedTime
 	});
 	$('#messages').append(html);
+	scrollToBottom();
 	// var formattedTime = moment(message.createdAt).format('h:mma');
 	// console.log('New Message:', JSON.stringify(message, undefined, 2));
 	// var li = $('<li></li>');
@@ -33,6 +52,7 @@ socket.on('newLocationMessage', function(message){
 		url: message.url
 	});
 	$('#messages').append(html);
+	scrollToBottom();
 	// var formattedTime = moment(message.createdAt).format('h:mma');
 	// var li = $('<li></li>');
 	// var a = $('<a target="_blank">My Current Location</a>');
